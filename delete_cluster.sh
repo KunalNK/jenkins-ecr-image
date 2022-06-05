d4b2f706b61c70d413c00e7ba1bddb5f64f7ca9c
Kunal <knkillekar@gmail.com>
2022-06-05 16:17:09 +0530

    update

--- delete_cluster.sh
+++ delete_cluster.sh
@@ -2,4 +2,4 @@
 ecs-cli down --force --cluster-config ec2-tutorial --ecs-profile ec2-tutorial-profile
 
 aws ecr delete-repository \
-    --repository-name image_name
+    --repository-name image_name --force
