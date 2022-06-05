e4bebc23495b481a305eead0ae78f74f1c96af20
Kunal <knkillekar@gmail.com>
2022-06-05 14:33:18 +0530

    up

--- delete_cluster.sh
+++ delete_cluster.sh
@@ -2,5 +2,5 @@
 ecs-cli down --force --cluster-config ec2-tutorial --ecs-profile ec2-tutorial-profile
 
 aws ecr delete-repository \
-    --repository-name jenkins-cicd \
+    --repository-name jenkins-ecs \
     --force
